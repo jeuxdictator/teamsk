894818e0fb5d99a301ae2ef50d1cfa94343eb07c
jeuxdictator <45318464+jeuxdictator@users.noreply.github.com>
2019-04-05 22:12:42 +0200

    Add files via upload

---
+++ teamsk.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const teamsk = client.guilds.get("563771921812946964")
+
+client.login(process.env.TOKEN)
+
+client.on("ready", () => {
+    console.log("connected")
+    client.user.setPresence({
+        game: { 
+            name: `Les membres de la SK_ !`,
+            type: 'WATCHING' 
+        },
+        status: 'dnd' 
+    })
+});
+
+
+
+client.on(`message`, message =>{
+    if(message.author.id === client.user.id) return 
+    if(message.author.bot) return
+    if(message.channel.type === "dm") return
+    if(message.guild.id !== "515900348922331136") return
+    console.log("111")
+
+    if(client.guilds.get("563771921812946964").channels.filter(z => z.type === "text" && z.name === message.channel.name).size !==0){
+        console.log("2")
+        const embed = new Discord.RichEmbed()
+        .setColor(`RANDOM`)
+        .addField(message.author.tag + " : ", message.content)
+        .setTimestamp()
+        .setFooter(client.user.tag +" - Jéhèndé#3800")
+        .setAuthor("-", message.author.avatarURL)
+        client.guilds.get("563771921812946964").channels.filter(z => z.type === "text" && z.name === message.channel.name).map(e => e.send(embed))
+    }else{
+        console.log("1")
+        client.guilds.get("563771921812946964").createChannel(message.channel.name, 'text', [{
+            id: message.guild.id,
+            deny: ['MANAGE_MESSAGES', 'SEND_MESSAGES']
+        }]).then(z =>{
+            const embed = new Discord.RichEmbed()
+            .setColor(`RANDOM`)
+            .addField(message.author.tag + " : ", message.content)
+            .setTimestamp()
+            .setFooter(client.user.tag +" - Jéhèndé#3800")
+            .setAuthor("-", message.author.avatarURL)
+            z.send(embed)
+        })
+        .catch(console.error);
+    }
+});
